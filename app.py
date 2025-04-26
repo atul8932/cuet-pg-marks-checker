62aa36286f4db81ad3cd5af0316cd34efdafc018
atul8932 <111909649+atul8932@users.noreply.github.com>
2025-04-26 20:50:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 import pandas as pd
 
 st.set_page_config(page_title="CUET PG Checker", layout="wide")
-st.title("📘 CUET PG Answer Key Hacker")
+st.title("📘 CUET PG Answer Key Checker")
 
 response_sheet_file = st.file_uploader("📄 Upload Response Sheet PDF", type=["pdf"])
 answer_key_file = st.file_uploader("🔐 Upload Answer Key PDF", type=["pdf"])
